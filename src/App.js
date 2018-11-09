2429f25cfc0cc49d49beaabd60c0a1b997874dd1
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-11-09 16:19:20 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Parallax, Background } from 'react-parallax'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Parallax
+            blur={1}
+            bgImage={require('./utah.jpg')}
+            bgImageAlt="the cat"
+            strength={700}
+            renderLayer={percentage => (
+              <div>
+                <div
+                    style={{
+                        position: 'absolute',
+                        background: `rgba(255, 125, 0, ${percentage * 1})`,
+                        left: '25%',
+                        top: '50%',
+                        borderRadius: '50%',
+                        width: percentage * 500,
+                        height: percentage * 500,
+                        transform: "translate(-50%, -50%)"
+                    }}
+                />
+                <div
+                    style={{
+                        position: 'absolute',
+                        background: `rgba(255, 125, 0, ${percentage * 1})`,
+                        left: '50%',
+                        top: '50%',
+                        borderRadius: '50%',
+                        width: percentage * 1500,
+                        height: percentage * 500,
+                        transform: "translate(-50%, -50%)"
+                    }}
+                />
+                <div
+                    style={{
+                        position: 'absolute',
+                        background: `rgba(255, 125, 0, ${percentage * 1})`,
+                        left: '75%',
+                        top: '50%',
+                        borderRadius: '50%',
+                        width: percentage * 500,
+                        height: percentage * 500,
+                        transform: "translate(-50%, -50%)",
+                    }}
+                />
+
+              </div>
+          )}
+        >
+            <div style={{ height: '150vh' }} />
+        </Parallax>
       </div>
     );
   }
